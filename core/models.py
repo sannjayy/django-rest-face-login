7a8edac38930ba0bc95bce2d06f50e28cc8f8877
Sanjay <znasofficial@gmail.com>
2021-09-28 19:30:13 +0530

    update models.py

--- core/models.py
+++ core/models.py
@@ -9,7 +9,7 @@ class User(AbstractUser):
     @property
     def screen_lock_img_url(self):
         try:
-            url = self.screen_lock_img.url
+            url = '/app'+self.screen_lock_img.url
         except:
             url = None
 
@@ -32,7 +32,7 @@ class LoginAttempt(models.Model):
     @property
     def image_url(self):
         try:
-            url = self.image.url
+            url = '/app'+self.image.url
         except:
             url = None
 
